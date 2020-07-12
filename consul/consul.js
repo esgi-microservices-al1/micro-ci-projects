48278b7c64b299ed51a1f3ff176ee8e10a0ec2d0
Pierre <pkettmus@gmail.com>
2020-07-12 16:23:31 +0200

    add consul

--- consul/consul.js
+++ consul/consul.js
@@ -1,31 +1,26 @@
 'use strict';
 
 const port = Number(process.env.CONSUL_PORT);
+const RegistrationDetails = require('./registration.details');
 
 const consul = require('consul')({
     host: process.env.CONSUL_HOST,
-    port: Number(process.env.CONSUL_PORT)
+    port: Number(process.env.CONSUL_PORT),
 });
-
 
 console.log(process.env.CONSUL_HOST + ':' + process.env.CONSUL_PORT);
 
-const uuid = require('uuid');
+const tags = [
+    'traefik.enable=true',
+    'traefik.frontend.entryPoints=http',
+    `traefik.frontend.rule=PathPrefixStrip:/${process.env.APPLICATION_NAME}-ci/`
+];
 
-const details = {
-    name: process.env.APPLICATION_NAME,
-    address: process.env.HOST || 'localhost',
-    port: Number(process.env.PORT),
-    id: `${process.env.APPLICATION_NAME}-${uuid.v4()}`,
-    check: {
-        ttl: '10s',
-        deregister_critical_service_after: '1m'
-    },
-    tags : [
-        'Test Consul For Project Micro Service',
-    ],
-    token: process.env.CONSUL_TOKEN
-};
+const details = new RegistrationDetails(process.env.APPLICATION_NAME, 
+    process.env.HOST || 'localhost', process.env.PORT, 
+    tags, process.env.CONSUL_TOKEN);
+
+
 
 
 const register = () => {
@@ -38,32 +33,28 @@ const register = () => {
         }
         else { console.log('consul good zkqhdkqz'); }
     });
-};
 
-const healthCheck = (serviceId) => {
-    console.log('HEALTHCHECK');
-    const jsonId = { id: `service:${serviceId}`, token: process.env.CONSUL_TOKEN };
+    const jsonId = { id: `service:${details.id}`, token: process.env.CONSUL_TOKEN || null };
 
     setInterval(() => {
         consul.agent.check.pass(jsonId, (err) => {
             if (err) { console.log(err.message); }
         })
     }, 5000);
+    
+    process.on('exit', unregister);
+    process.on('SIGINT', unregister);
+    process.on('uncaughtException', unregister);
+    
 };
 
-const unregister = (consul_id) => {
-    console.log(consul_id);
-    const unregisterService = () => {
-        consul.agent.service.deregister(consul_id, () => {
-            process.exit(err ? 1 : 0);
-        });
-    };
-    unregisterService();
-    process.on('exit', unregisterService);
-    process.on('SIGINT', unregisterService);
-    process.on('uncaughtException', unregisterService);
+function unregister() {
+    const toUnregister = {
+        id: details.id,
+        token: process.env.CONSUL_TOKEN || null };
+    consul.agent.service.deregister(toUnregister, (err) => {
+        process.exit(err ? 1 : 0);
+    });
 }
 
 module.exports.register = register;
-module.exports.healthcheck = healthCheck;
-module.exports.unregister = unregister;
