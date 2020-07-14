317d77070655af9923a118b5c266f06f79a6ab5d
Pierre <pkettmus@gmail.com>
2020-07-14 20:00:14 +0200

    add swagger for api

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 'use strict';
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
 class RouterBuilder {
     build(app) {
+        app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
         app.use('/project', require('./project.router'));
     }
 }
