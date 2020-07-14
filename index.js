2d513165f2272a649c4656f614da347e5985426b
Pierre <pkettmus@gmail.com>
2020-07-14 15:19:08 +0200

    comment rabbitmq

--- index.js
+++ index.js
@@ -32,14 +32,14 @@ mongoose
 
 RouterBuilder.build(app)
 
-try {
-    console.log("\nTrying to consume\n");
-    ConsumerService.consumeWebHookQueue(process.env.AMQP_WEBHOOK_QUEUE_NAME);
-    ConsumerService.consumeSchedulerQueue(process.env.AMQP_SCHEDULER_QUEUE_NAME);
-} catch (ex) {
-    console.log('Consumer Error');
-    console.log(ex);
-}
+// try {
+//     console.log("\nTrying to consume\n");
+//     ConsumerService.consumeWebHookQueue(process.env.AMQP_WEBHOOK_QUEUE_NAME);
+//     ConsumerService.consumeSchedulerQueue(process.env.AMQP_SCHEDULER_QUEUE_NAME);
+// } catch (ex) {
+//     console.log('Consumer Error');
+//     console.log(ex);
+// }
 
 consul.register();
 
